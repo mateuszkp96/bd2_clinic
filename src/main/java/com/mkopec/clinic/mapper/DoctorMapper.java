67633375eb01ebb7a69d56d1280d7e8b432109e9
mateuszkp96 <mateuszkp96@gmail.com>
2019-05-31 00:48:18 +0200

    doctor and specialization mappers added

---
+++ src/main/java/com/mkopec/clinic/mapper/DoctorMapper.java
@@ -0,0 +1,25 @@
+package com.mkopec.clinic.mapper;
+
+import com.mkopec.clinic.domain.Doctor;
+import com.mkopec.clinic.dtos.DoctorDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public abstract class DoctorMapper {
+
+    @Autowired
+    protected SpecializationMapper specializationMapper;
+
+    @Mappings({
+            @Mapping(target = "firstname", source = "name"),
+            @Mapping(target = "specialization", expression = "java(specializationMapper.toDoctorSpecializationDTOs(doctor.getSpecializations()))")
+    })
+    public abstract DoctorDTO toDoctorDTO(Doctor doctor);
+
+    public abstract List<DoctorDTO> toDoctorDTOs(List<Doctor> doctorList);
+}
