769a97f42dd02b720861629fd2bf12f2cf707fd7
mateuszkp96 <mateuszkp96@gmail.com>
2019-05-31 20:09:08 +0200

    Shift repository and service added

---
+++ src/main/java/com/mkopec/clinic/repository/ShiftRepository.java
@@ -0,0 +1,13 @@
+package com.mkopec.clinic.repository;
+
+import com.mkopec.clinic.domain.Doctor;
+import com.mkopec.clinic.domain.Shift;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ShiftRepository extends JpaRepository<Shift, Long> {
+    List<Shift> findByDoctor(Doctor doctor);
+}
